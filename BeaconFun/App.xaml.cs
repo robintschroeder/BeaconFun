c831038847d3b6becb3dd1426ab6c02a2eeb71d3
Robin Schroeder <Robintschroeder@gmail.com>
2024-02-29 20:28:26 -0600

    init

---
+++ BeaconFun/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BeaconFun;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ BeaconFun/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BeaconFun;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
